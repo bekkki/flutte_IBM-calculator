59bede99cf3431a7ab796a6429ad2e8e4a109ebb
amanja4274 <amanja4274@gmail.com>
2022-01-25 02:57:28 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bmi/screens/home.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
